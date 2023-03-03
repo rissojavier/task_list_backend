78b61426a0a200a968d8a0342149778f3fcaf595
Javier Risso <88810285+rissojavier@users.noreply.github.com>
2023-03-03 13:47:47 -0400

    Update db.js

--- config/db.js
+++ config/db.js
@@ -6,7 +6,6 @@ const conectarDB = async () => {
         const db = await mongoose.connect(process.env.MONGO_URI, {
                 useNewUrlParser: true,
                 useUnifiedTopology: true,
-                maxTimeMS: 300000
             });
 
         const url = `${db.connection.host}:${db.connection.port}`;
